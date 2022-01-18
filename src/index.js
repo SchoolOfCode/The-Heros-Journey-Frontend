ceb49de74d86b10394966c6408aeccee0ca224f1
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-01-18 16:37:50 +0000

    adding redirection

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/';
-import Stage1 from './Components/Stage 1';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
