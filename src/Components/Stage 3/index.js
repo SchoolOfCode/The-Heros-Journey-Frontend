91f70f56697e21dddce22c74a693abc44f880c96
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-01-19 17:53:21 +0000

    fully responsive mvp 2

--- src/Components/Stage 3/index.js
+++ src/Components/Stage 3/index.js
@@ -1,31 +1,31 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import Input from "../Input";
-import Text from "../Text";
-import Padlock from "../Padlock";
-import SideNav from "../Sidenav";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Input';
+import Text from '../Text';
+import Padlock from '../Padlock';
+import SideNav from '../Sidenav';
 
 // Stage 3 - Imposter Syndrome stage - Hole in the ship Weeks 4-5 Backend Node, SQL
 // stage 3
 
 function Stage3() {
-  const [password, setPassword] = useState("");
-  const [link, setLink] = useState("/stage3");
+  const [password, setPassword] = useState('');
+  const [link, setLink] = useState('/stage3');
 
   function handleChange(e) {
     setPassword(e.target.value);
-    if (e.target.value === "VALUES") {
-      setLink("/stage4");
+    if (e.target.value === 'VALUES') {
+      setLink('/stage4');
     } else {
-      setLink("/stage3");
+      setLink('/stage3');
     }
   }
 
   function submit() {
-    if (password !== "VALUES") {
-      return alert("Access Denied");
+    if (password !== 'VALUES') {
+      return alert('Access Denied');
     } else {
-      alert("Permission granted");
+      alert('Permission granted');
     }
   }
 
@@ -39,47 +39,40 @@ function Stage3() {
         backgroundSize: `100vh`,
       }}
     >
-      <SideNav 
-        title="Houston, we have a problem!" 
+      <SideNav
+        title="Houston, we have a problem!"
         description="Getting out of your comfort zone and jumping straight into the unknown is an overwhelming feeling.
-      'What am I doing here?' is a question you may ask yourself as you begin to self-doubt and be filled with intrusive thoughts..." 
-      stageDesc="Back-end"/>
-  
+      'What am I doing here?' is a question you may ask yourself as you begin to self-doubt and be filled with intrusive thoughts..."
+        stageDesc="Back-end"
+      />
+
       <Text
         text="INSERT INTO spaceship"
         style={{
-
           position: 'fixed',
           top: '25vh',
-          left: '32vw',
           opacity: 1,
+          width: '43vh',
           fontSize: '2vh',
           fontWeight: 'bold',
           color: 'white',
-
         }}
       />
       <Text
         text="(SQL)"
         style={{
-
           position: 'fixed',
-          top: '28vh',
-          left: '34vw',
           opacity: 1,
           fontSize: '2vh',
           fontWeight: 'bold',
           color: 'white',
-
         }}
       />
       <Text
         text="($1, $2) RETURNING"
         style={{
-
           position: 'fixed',
           bottom: '25vh',
-          right: '30vw',
           opacity: 1,
           fontSize: '2vh',
           fontWeight: 'bold',
@@ -89,21 +82,18 @@ function Stage3() {
       <Text
         text="password"
         style={{
-
           position: 'fixed',
           bottom: '23vh',
-          right: '32vw',
           opacity: 1,
           fontSize: '2vh',
           fontWeight: 'bold',
           color: 'white',
-
         }}
       />
       <Input
         placeholder="Password"
         handleChange={handleChange}
-        style={{ position: "fixed", bottom: "8vh", right: "15vh" }}
+        style={{ position: 'fixed', bottom: '8vh', right: '15vh' }}
       />
       <Link onClick={submit} to={link}>
         <Padlock />
--- src/Components/Stage 4/index.js
+++ src/Components/Stage 4/index.js
@@ -1,55 +1,55 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import Click from "../Click";
-import Input from "../Input";
-import Padlock from "../Padlock";
-import SideNav from "../Sidenav";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Click from '../Click';
+import Input from '../Input';
+import Padlock from '../Padlock';
+import SideNav from '../Sidenav';
 
 // Stage 4 - Heroes journey - transformative - Floating in space Weeks 7-8 React
 
 function Stage4() {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
 
-  const [link, setLink] = useState("/stage4");
+  const [link, setLink] = useState('/stage4');
 
   useEffect(() => {
     console.log(
-      "I wonder what happens if I click around a bit and USE some of these objects..."
+      'I wonder what happens if I click around a bit and USE some of these objects...'
     );
   }, []);
 
   function handleChange(e) {
     setPassword(e.target.value);
-    if (e.target.value.toLowerCase() === "hooks") {
-      setLink("/stage5");
+    if (e.target.value.toLowerCase() === 'hooks') {
+      setLink('/stage5');
     } else {
-      setLink("/stage4");
+      setLink('/stage4');
     }
   }
 
   function submit() {
-    if (password.toLowerCase() !== "hooks") {
-      return alert("Access Denied");
+    if (password.toLowerCase() !== 'hooks') {
+      return alert('Access Denied');
     } else {
-      alert("Permission granted");
+      alert('Permission granted');
     }
   }
 
   function handleClick(text) {
     switch (text) {
-      case "1":
-        console.log("useQuestionMark");
+      case '1':
+        console.log('useQuestionMark');
         break;
-      case "2":
-        console.log("useHelmet");
+      case '2':
+        console.log('useHelmet');
         break;
-      case "3":
-        console.log("usePlanet");
+      case '3':
+        console.log('usePlanet');
         break;
-      case "4":
-        console.log("useComet");
-      case "5":
-        console.log("useSuperhero");
+      case '4':
+        console.log('useComet');
+      case '5':
+        console.log('useSuperhero');
         break;
       default:
     }
@@ -74,12 +74,12 @@ function Stage4() {
         text="1"
         handleClick={handleClick}
         style={{
-          position: "fixed",
-          top: "28.5vh",
-          left: "27.5vw",
-          height: "9vh",
-          width: "5vh",
-          cursor: "pointer",
+          position: 'fixed',
+          top: '28.5vh',
+          marginRight: '79vh',
+          height: '9vh',
+          width: '6vh',
+          cursor: 'pointer',
           opacity: 0,
         }}
       />
@@ -87,12 +87,14 @@ function Stage4() {
         text="2"
         handleClick={handleClick}
         style={{
-          position: "fixed",
-          top: "40vh",
-          left: "31vw",
-          height: "12vh",
-          width: "10vh",
-          cursor: "pointer",
+          position: 'fixed',
+          top: '39vh',
+          marginRight: '61.5vh',
+          height: '14vh',
+          width: '11vh',
+          cursor: 'pointer',
+          borderRadius: '40%',
+          transform: 'rotate(-25deg)',
           opacity: 0,
         }}
       />
@@ -100,13 +102,14 @@ function Stage4() {
         text="3"
         handleClick={handleClick}
         style={{
-          position: "fixed",
-          bottom: "11vh",
-          right: "36vw",
-          height: "15vh",
-          width: "22vh",
-          borderRadius: "150px 150px 150px 1px",
-          cursor: "pointer",
+          position: 'fixed',
+          bottom: '11.5vh',
+          marginLeft: '26.5vh',
+          height: '14vh',
+          width: '26vh',
+          borderRadius: '50%',
+          transform: 'rotate(-25deg)',
+          cursor: 'pointer',
           opacity: 0,
         }}
       />
@@ -114,39 +117,40 @@ function Stage4() {
         text="4"
         handleClick={handleClick}
         style={{
-          position: "fixed",
-          top: "45vh",
-          right: "40vw",
-          height: "10vh",
-          width: "11vh",
-          transform: "skew(50deg)",
+          position: 'fixed',
+          top: '45vh',
+          marginLeft: '25.5vh',
+          height: '11vh',
+          width: '11vh',
+          transform: 'skew(40deg)',
           opacity: 0,
-          cursor: "pointer",
+          cursor: 'pointer',
         }}
       />
       <Click
         text="5"
         handleClick={handleClick}
         style={{
-          position: "fixed",
-          top: "10vh",
-          right: "35vw",
-          height: "20vh",
-          width: "25vh",
-          transform: "skew(60deg)",
-          cursor: "pointer",
+          position: 'fixed',
+          top: '8vh',
+          marginLeft: '40.5vh',
+          height: '25vh',
+          width: '40vh',
+          transform: 'skew(50deg)',
+          borderRadius: '50% 50% 40% 50%',
+          cursor: 'pointer',
           opacity: 0,
         }}
       />
       <Input
         placeholder="Password"
         handleChange={handleChange}
-        style={{ position: "fixed", bottom: "8vh", right: "15vh" }}
+        style={{ position: 'fixed', bottom: '8vh', right: '15vh' }}
       />
       <Link onClick={submit} to={link}>
         <Padlock />
       </Link>
-      <p style={{ position: "fixed", bottom: "10vh", right: "10vh" }}>
+      <p style={{ position: 'fixed', bottom: '10vh', right: '10vh' }}>
         Hint: Console
       </p>
     </div>
