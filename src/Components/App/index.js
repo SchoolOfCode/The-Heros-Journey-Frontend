e959692a7c41f2feca207da156a24c85a33c051e
Indya 2 <indya2@Steves-MacBook-Pro.local>
2022-01-19 13:35:10 +0000

    stage 1 to 3

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,21 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import Stage1 from '../Stage 1/';
-import Stage2 from '../Stage 2/';
-import Stage3 from '../Stage 3/';
-import Stage4 from '../Stage 4/';
-import Stage5 from '../Stage 5/';
+import { Routes, Route } from "react-router-dom";
+import StartScreen from "../StartScreen/";
+import HowToPlay from "../StartScreen/HowToPlay";
+import Stage1 from "../Stage 1/";
+import Stage2 from "../Stage 2/";
+import Stage3 from "../Stage 3/";
+import Stage4 from "../Stage 4/";
+import Stage5 from "../Stage 5/";
 
 function App() {
   return (
     <div
       className="stage"
       style={{
-        backgroundImage: `url("https://cdn.pixabay.com/photo/2018/02/01/06/40/rocket-3122690_960_720.png")`,
+        backgroundColor: "white",
         backgroundRepeat: `no-repeat`,
         backgroundSize: `100vw 100vh`,
       }}
     >
       <Routes>
+        <Route path="startscreen" element={<StartScreen />} />
+        <Route path="howtoplay" element={<HowToPlay />} />
         <Route path="stage1" element={<Stage1 />} />
         <Route path="stage2" element={<Stage2 />} />
         <Route path="stage3" element={<Stage3 />} />
---
+++ src/Components/StartScreen/StartHeading/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+function StartHeading({ text, style }) {
+  return <h1 style={style}>{text}</h1>;
+}
+
+export default StartHeading;
